4054aaa4d962f7dfe499f87d59782bc801f135c1
francoponsfunes <139824057+francoponsfunes@users.noreply.github.com>
2026-08-22 14:29:06 -0300

    Delete core.js

--- core.js
+++
@@ -1,7 +0,0 @@
-// ============================================================================
-// CORE.JS
-// Elementos base del juego
-// ============================================================================
-
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
